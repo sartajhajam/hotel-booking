035b925b322a9915647a4b705d6f281d16a62fa8
sartaj <hajamsaj@gmail.com>
2025-04-29 14:36:41 +0530

    added AllRooms Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Route } from 'react-router-dom'
 import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
       <div className='min-h-[70vh'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
           
         </Routes>
 
---
+++ src/pages/AllRooms.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AllRooms = () => {
+  return (
+    <div className='flex  flex-col-reverse lg:flex-row items-start justify-between pt-28 mf:pt-35 px-4 md:px-16 lg:px-24 xl:px-32 '>
+      <div>
+        <div>
+          <h1 className='font -playfair text-4xl md:text-[40px]'>Hotel  Rooms</h1>
+          <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>The advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
+        </div>
+      </div>
+      {/* Filters */}
+      <div>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default AllRooms
