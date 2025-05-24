957465335bf3d57f5b851e05f1537c2e8e88b169
sartaj <hajamsaj@gmail.com>
2025-05-24 15:51:39 +0530

    added Room_Details

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -8,16 +8,19 @@ const RoomDetails = () => {
   const [mainImage, setMainImage] = useState(null);
 
   useEffect(() => {
-    const room = roomsDummyData.find((room) => {
-      if (room.id === id) {
-        setRoom(room);
-        setMainImage(room.images[0]);
-      }
-    }
+    const room = roomsDummyData.find(room => room._id === id) 
+        room && setRoom(room)
+        room && setMainImage(room.images[0]);      
+    }, [])
 
-  },[])
-  return (
-    <div>
+  return room &&  (
+    <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+      {/* Room Details */}
+      <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
+        <h1 className='text-3xl md:text-4xl font-plafair'>{room.hotel.name} <span className='font-inter text-sm'> {room.roomType}</span></h1>
+        <p>20 % OFF</p>
+
+      </div>
       
     </div>
   )
