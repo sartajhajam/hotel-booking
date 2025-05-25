77a60c62c5c74c951fa31234ff09dd38b2ab6fcd
sartaj <hajamsaj@gmail.com>
2025-05-25 16:03:25 +0530

    added MyBookingsPage

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Title from '../components/Title'
+import { assets, userBookingsDummyData } from '../assets/assets'
 
 const MyBookings = () => {
+
+  const[bookings, setBookings] = React.useState(userBookingsDummyData);
   return (
     <div>
       <Title title='My Bookings' subTitle='Easily manage your past,current and upcoming hotel reservations in one place.Plan your trips seamlessly with just few clicks ' align='left'/>
@@ -12,6 +15,28 @@ const MyBookings = () => {
           <div className='w-1/3'>Date & Timings</div>
           <div className='w-1/3'>Payment</div>
         </div>
+
+        {bookings.map((booking, index) => (
+          <div key={booking._id}className='grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr]w-full border-b border-gray-300 py-6 first:border-t' >
+            {/* Hotel Details */}
+            <div> 
+              <img src={booking.room.hotel.images[0]} alt="Hotel-img" className='min-md:w-44 rounded-lg shadow object-cover' /> 
+              <div>
+                <p className='font-playfair text-2xl' >{booking.room.hotel.name}
+                 <span className='font-inter text-sm'>({booking.room.roomType})</span>
+                 </p>
+                 <div className='flex items-center gap-1 text-gray-500  text-sm'>
+                  <img src={assets.locationIcon} alt="location-icon"/>
+                  <span>{booking.hotel.address}</span>
+                  </div>
+
+                          </div>
+            </div>
+            {/* Date & Timings */}
+            <div> </div>
+            {/* Payment Status */}
+            <div> </div>
+            }
       </div>
       
     </div>
