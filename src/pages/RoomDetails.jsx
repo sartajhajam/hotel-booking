08b97c41c6c2a3faa086b3d4d0be119be383d6e3
sartaj <hajamsaj@gmail.com>
2025-05-25 14:39:26 +0530

    added Room_search_bar

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -75,9 +75,23 @@ const RoomDetails = () => {
       <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] rounded-xl p-6 mx-auto mt-16 max-w-6xl'>
 
         <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+        <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
           <div className='flex flex-col '>
             <label htmlFor='checkInDate' className='font-medium '>Check-In </label>
             <input type="date" id='checkInDate'placeholder='Check-In'className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none required'/>
+          </div>
+
+          <div className='flex flex-col '>
+            <label htmlFor='checkOutDate' className='font-medium '>Check-Out </label>
+            <input type="date" id='checkOutDate'placeholder='Check-Out'className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none required'/>
+          </div>
+          <div className='w-px h-15 bg-gray-300/70 max-md:hidden'>
+
+          </div>
+
+          <div className='flex flex-col '>
+            <label htmlFor='guests' className='font-medium '>Guests</label>
+            <input type="number" id='guests'placeholder='0'className='max-w-20 ounded border border-gray-300 px-3 py-2 mt-1.5 outline-none'required/>
           </div>
 
         </div>
