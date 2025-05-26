22f8f3cd81437661c4d8e813b6d070d4e7045f2f
sartaj <hajamsaj@gmail.com>
2025-05-26 13:50:02 +0530

    added HotelReg_Component_cc

--- src/components/HotelReg.jsx
+++ src/components/HotelReg.jsx
@@ -11,8 +11,27 @@ const HotelReg = () => {
           <img src={assets.closeIcon} alt="close-icon" className='w-4 h-4 absolute top-4 right-4 cursor-pointer' />
           <p className='text-2xl font-semibold mt-6 '>Register Your Hotel </p>
 
+          {/*Hotel Name */}
+          <div className='w-full mt-4'>
+            <label htmlFor='name' className='font-medium text-gray-500'>
+              Hotel Name
+            </label>
+            <input type='text' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light required'></input>
+
+          </div>
+
+          {/*Hotel Phone */}
+
+          <div className='w-full mt-4'>
+            <label htmlFor='contact' className='font-medium text-gray-500'>
+              Phone
+            </label>
+            <input id='contact' type='text' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light required'></input>
+
+          </div>
+
         </div>
-        
+
 
       </form>
     </div>
