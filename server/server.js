1cb97b2743f900f1931d4680f638bddc4b75fea2
sartaj <hajamsaj@gmail.com>
2025-05-29 16:36:10 +0530

    added mongodb connec

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
+import connectDB from './config/db.js';
+
+// calling the connectDB function to establish a connection to the database
+connectDB();
 
 const app = express();
 
@@ -8,7 +12,7 @@ app.use(cors());  //enable cross origin resource sharing
 
 
 app.get('/', (req, res) => 
-  res.send('Api is working fine !'))
+  res.send('Api is working fine now  !'))
   
 const PORT = process.env.PORT || 3000;
 
