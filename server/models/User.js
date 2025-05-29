8bffd84d669c6df1aa1c993ec8d7f51e175b353b
sartaj <hajamsaj@gmail.com>
2025-05-29 16:54:59 +0530

    added UserModal

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+// create a user schema
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String, 
+    required: true
+  },  // Using String for _id to allow custom IDs
+    
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required:true},
+  role: {
+    type: String,
+    enum: ['hotelOwner', 'user'],
+    default: "user"},
+
+  recentSearchCities:[{
+    type:String,
+    required:true
+  }],
+
+},{timestamps: true});
+
+// create a user model
+
+const User = mongoose.model("User", userSchema);
+
+// export the user model
+export default User;
+// The user model can now be used to interact with the users collection in MongoDB
+
+
+
+  
