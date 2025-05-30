4381f182928f9de95b06a58e6f37a9fcce4a23aa
sartaj <hajamsaj@gmail.com>
2025-05-30 13:54:10 +0530

    added UserController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,10 +3,15 @@
 
 export const getUserData = async (req, res) => {
   try {
+    const role = req.user.role;
+    const recentSearchCities = req.user.recentSearchCities;
+    res.json({success :true, role, recentSearchCities});   
+
 
   } catch (error){
+    res.json({success :false, message: error.message});
 
-    
+
   }
 
 }
