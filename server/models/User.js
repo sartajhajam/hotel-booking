c883cbdfeba28e389851661ad2b92c3d3abaf7d1
sartaj <hajamsaj@gmail.com>
2025-05-30 13:25:21 +0530

    modal_changes

--- server/models/User.js
+++ server/models/User.js
@@ -16,10 +16,6 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
   image: {
     type: String,
     required:true},
