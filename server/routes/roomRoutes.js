e509ece4f82df15dd0760790692a6463b5f49579
sartajhajam <hajamsaj@gmail.com>
2025-05-31 15:59:34 -0700

    added roomToggle

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
-import {createRoom} from "../controllers/roomController.js"
+import {createRoom, getOwnerRooms, getRooms ,toggleRoomAvailability} from "../
 
 const roomRouter = express.Router();
 
 roomRouter.post('/',upload.array("images",4),protect,createRoom)
+roomRouter.get('/',getRooms)
+roomRouter.get('/owner',protect,getOwnerRooms)
+roomRouter.get('/t toggle-availability',protect,t toggleRoomAvailability)
 
 export default roomRouter
