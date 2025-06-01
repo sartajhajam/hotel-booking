066263b45634a690d2c765c3c788b8744344931d
sartajhajam <hajamsaj@gmail.com>
2025-06-01 14:38:51 -0700

    bookingController

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,15 @@
+import Booking from "../models/booking.js";
+// function to check availaity of room
+const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
+  try {
+    const bookings = await Booking.find({
+      room,
+      checkInDate: { $lte: checkOutDate },
+      checkOutDate: { $gte: checkInDate },
+    });
+    const isAvailable = bookings.length === 0;
+    return isAvailable;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
