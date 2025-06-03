7a4682ab1f559cc0a8a41eb2e9c34ce8b4117ec0
sartajhajam <hajamsaj@gmail.com>
2025-06-02 22:30:03 -0700

    added useAppContext

---
+++ client/src/context/appContext.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { createContext, useContext } from "react";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+const AppContext = createContext();
+
+ const useAppContext = () => createContext();
+
+export const AppProvider = ({ children }) => {
+    const value = {
+
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext);
